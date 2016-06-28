f5166b514279e82e93ee022e820954cd52893522
Christoph Berger <christophberger@users.noreply.github.com>
2016-06-28 22:01:17 +0200

    Text almost finished. Rest of Caddy example and all of Script Plugin still missing. Pics and animations still missing. Code still missing.

--- plugins.go
+++ plugins.go
@@ -15,22 +15,130 @@ Comments and code in this file are used for describing and explaining a particul
 -->
 
 +++
-title = ""
-description = ""
+title = "Plugins in Go"
+description = "Go is a statically compiled language. No dynamic libraries can be loaded at runtime, nor does the runtime support compiling Go on the fly. Still, there is a number of ways of creating and using plugins in Go."
 author = "Christoph Berger"
 email = "chris@appliedgo.net"
-date = "2016-00-00"
-publishdate = "2016-00-00"
-domains = [""]
-tags = ["", "", ""]
-categories = ["Tutorial"]
+date = "2016-06-30"
+publishdate = "2016-06-30"
+domains = ["Architecture"]
+tags = ["Plugin", "", ""]
+categories = ["Survey"]
 +++
 
-### Summary goes here
+Go is a statically compiled language. No dynamic libraries can be loaded at runtime, nor does the runtime support compiling Go on the fly. Still, there is a number of ways of creating and using plugins in Go.
 
 <!--more-->
 
-## Intro goes here
+## The case for plugins in Go
+
+Plugins are useful for extending an application's feature list - but in Go, compiling a whole app from source is fast and easy, so why should anyone bother with plugins in Go?
+
+* First, loading a plugin at runtime may be a requirement in the app's technical specification.
+* Second, fast compilation and plugins are no contradiction. Go plugins can be created to be compiled into the binary - we'll look at an example of this later.
+
+This article is a quick survey of plugin architectures and techniques in Go.
+
+
+## Plugin criteria
+
+Here is a wishlist for the ideal plugin architecture:
+
+* **Speed:** Calling a plugin's methods must be fast. The slower the method call is, the more is the plugin restricted to implementing only big, long-running, rarely called methods.
+* **Reliability:** Plugins should not fail or crash, and if they do, recovery must be possible, fast, easy, and complete.
+* **Security:** Plugins should be secured against tampering, for example, through code signing.
+* **Ease of use:** The plugin programmer should not be burdened with a complicated, error-prone plugin API.
+
+The ideal plugin architecture should meet all of the above criteria, but in real life there is usually one or another concession to make. This becomes immediately clear when we look at the question that should be the first one when deciding upon a plugin architecture:
+
+*Shall the plugins run inside the main process, or rather be separate processes?*
+
+
+## In-process vs separate processes
+
+Both approaches have advantages and disadvantages, and as we'll see, one approach's disadvantage may be the other's advantage.
+
+
+### Advantages of in-process plugins
+
+* **Speed:** Method calls are as fast as can be.
+* **Reliability:** The plugins are available as long as the main process is available. An in-process plugin cannot suddenly become unavailable at runtime.
+* **Easy deployment:** The plugin gets deployed along with the binary, either baked right in, or (only in non-Go languages for now) as a dynamic shared library that can be loaded either at process start or during runtime.
+* **Easy runtime management:** No need for discovering, starting, or stopping a plugin process. No need for health checks. (Does the plugin process still live? Does it hang? Does it need a restart?)
+
+
+### Advantages of plugins as separate processes
+
+* **Resilience:** A crashing plugin does not crash main process.
+* **Security:** A plugin in a separate process cannot mess with internals of the main process.
+* **Flexibility (part 1):** Plugins can be written in any language, as long as there is a library available for the plugin protocol.
+* **Flexibility (part 2):** Plugins can be activated and deactivated during runtime. It is even possible to deploy and activate new plugins without having to restart the main process.
+
+With these feature lists in mind, let's look at a couple of different plugin solutions for the Go language.
+
+## Plugin approaches in Go
+
+As mentioned before, Go lacks an option for loading shared libraries at runtime, and so a variety of alternate approaches have been created. Here are the ones I could find through two quick searches on GitHub and on Google, in no particular order:
+
+### External process with RPC via stdin/stdout
+
+#### Description
+
+This is perhaps the most straightforward approach:
+
+* Main process starts plugin process
+* Main process and plugin process are connected via stdin and stdout
+* Main process uses RPC ([Remote Procedure Call](https://en.wikipedia.org/wiki/Remote_procedure_call) via stdin/stdout connection
+
+#### Source/Example
+
+The blog post [Go Plugins are as Easy as Pie](http://npf.io/2015/05/pie/) introduced this concept to Go in May 2015. The accompanying `pie` package is [here](https://github.com/natefinch/pie), and if you ask me, this could be my favorite plugin approach just for the yummy pumpkin pie picture in the readme!
+
+
+### External process with RPC via network
+
+#### Description
+
+The main difference to the previous approach is the way how the RPC calls are implemented. Rather than using the stdin/stdout connection, the RPC calls can also be done via the (local) network.
+
+#### Source/Example
+
+The package [`go-plugin`](https://github.com/hashicorp/go-plugin) by Hashicorp utilizes `net/rpc` for connecting to the plugin processes. `go-plugin` is a rather heavyweight plugin system with lots of features, clearly able to attract developers of enterprise software who look for a complete and industry tested solution.
+
+
+### External process via message queue
+
+#### Description
+
+Message queue systems, especially the brokerless ones, provide a solid groundwork for creating plugin systems. My quick research did not return any MQ-based plugin solution, but this may well be due to the fact that not much is needed to turn a message queue system into a plugin architecture.
+
+#### Source/Example
+
+Remember my first two posts of this blog? I introduced the [nanomsg](http://nanomsg.org/) system and its Go implementation [Mangos](https://github.com/go-mangos/mangos). The nanomsg specification includes a set of predefined communication protocols covering many different scenarios: Pair, PubSub, Bus, Survey, Pipeline, and ReqRep. Some of them come in quite handy for communicating with plugins:
+
+* The ReqRep (Request-Reply) protocol can be used for mimicking RPC calls to a particular plugin. It is not the real RPC thing, however, as the sockets handle plain `[]byte` data only. So the main process and the plugins must take care of serializing and de-serializing the request and reply data.
+* The Survey protocol helps monitoring the status of all plugins at once. The main process sends a survey to all plugins, and the plugins respond if they can. If a plugin does not respond within the deadline, the main process can take measures to restart the plugin.
+*
+
+### In-process plugins, included at compile time
+
+#### Description
+
+Calling a package a plugin might seem debatable when it is compiled into the main application just like any other package. But as long as there is a plugin API defined that the plugin packages implement, and as long as the build process is able to pick up any plugin that has been added, there is nothing wrong with that.
+
+The advantages of in-process plugins--speed, reliability, ease of use--have been outlined above. As a downside, adding, removing, or updating a plugin requires compling and deploying the whole main application.
+
+
+#### Source/Example
+
+If you know the awesome Web server [Caddy](https://caddyserver.com/), you might already have seen a compile-time plugin system in action. On the donwload page, the first step lets you select from a dozen or so add-ons which get compiled into the caddy binary.
+
+
+### Script plugins via scripting API
+
+* Lua
+* http://awesome-go.com/#embeddable-scripting-languages
+
 
 ## The code
 */
